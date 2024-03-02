fc74d09770ebe87507d4530d3ce56ff93a158e7c
mentalNika <mandrygina798@mail.ru>
2024-03-03 00:36:52 +0300

    Banner component

--- app/page.js
+++ app/page.js
@@ -1,31 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Banner from "./components/Banner/Banner";
 
 export default function Home() {
   return (
     <main className="main">
-      <section className="banner">
-        <div className="banner__description">
-          <h1 className="banner__title">
-            Портал инди-игр от&nbsp;студентов Яндекс Практикума
-          </h1>
-          <div className="banner__text-block">
-            <p className="banner__text">
-              Студенты курсов разрабатывают свои игры на Unity, здесь мы их
-              публикуем. Вы можете играть прямо на сайте. А если у вас есть
-              аккаунт пользователя — получаете возможность голосовать за игры.
-            </p>
-          </div>
-          <a href="#popular" className="button banner__link">
-            Смотреть игры
-          </a>
-        </div>
-        <img
-          src="/images/banner-illustration.jpg"
-          alt="Рука, утопленная в желтом фоне"
-          className="banner__image"
-        />
-      </section>
+      <Banner />
       <section className="list-section">
         <h2 className="list-section__title" id="popular">
           Популярное
