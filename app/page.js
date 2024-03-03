8b299666e31734b740c379759669935d2817d6b9
mentalNika <mandrygina798@mail.ru>
2024-03-03 21:19:08 +0300

    CardList

--- app/page.js
+++ app/page.js
@@ -2,16 +2,22 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import "./globals.css";
 import Banner from "./components/Banner/Banner";
-import PopularCardsList from "./components/CardsList/PopularCardsList";
-import NewCardsList from "./components/CardsList/NewCardsList";
+import CardsList from "./components/CardsList/CardsList";
+import PopularCardsFragment from "./components/CardsList/PopularCardsFragment";
+import NewCardsFragment from "./components/CardsList/NewCardsFragment";
 import Promo from "./components/Promo/Promo";
 
 export default function Home() {
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id={"popular"} title={"Популярные"}>
+        <PopularCardsFragment />
+      </CardsList>
+
+      <CardsList>
+        <NewCardsFragment id={"new"} title={"Новые"} />
+      </CardsList>
       <Promo />
     </main>
   );
